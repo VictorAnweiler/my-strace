6aefbf05746666cb6310545ca16461cd4b5b059f
Théo champion <theo.champion@epitech.eu>
2017-04-13 14:28:01 +0200

    python generator

--- main.c
+++ main.c
@@ -5,22 +5,17 @@
 ** Login   <theo.champion@epitech.eu>
 ** 
 ** Started on  Wed Apr 12 11:13:34 2017 theo champion
-** Last update Wed Apr 12 17:13:20 2017 theo champion
+** Last update Thu Apr 13 12:55:03 2017 theo champion
 */
 
 #include "header.h"
 
 int	main(int argc, char **argv, char **env) {
-  pid_t	p_child;
 
   if (argc < 2) {
-    fprintf(stderr, "Usage: %s prog args\n", argv[0]);
+    fprintf(stderr, "Usage : %s [-s] [-p <pid>|<command>]", argv[0]);
     exit(1);
   }
 
-  p_child = fork();
-  if (p_child == 0)
-    return launch_child(argv, env);
-  else
-    return trace(p_child);
+  return (execve("./strace", argv, env));
 }
